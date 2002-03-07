1da7d281f3ad8dfcc427ee5265f1d83e4e8b54df
jason <jason>
2002-03-07 17:37:45 +0000

    Update to OpenSSH-3.1 on 3.0-stable branch

--- fatal.c
+++ fatal.c
@@ -1,7 +1,5 @@
-/*	$OpenBSD: key.h,v 1.17.2.1 2002/03/07 17:37:46 jason Exp $	*/
-
 /*
- * Copyright (c) 2000, 2001 Markus Friedl.  All rights reserved.
+ * Copyright (c) 2002 Markus Friedl.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,58 +21,20 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef KEY_H
-#define KEY_H
 
-#include <openssl/rsa.h>
-#include <openssl/dsa.h>
+#include "includes.h"
+RCSID("$OpenBSD: fatal.c,v 1.1.2.1 2002/03/07 17:37:46 jason Exp $");
 
-typedef struct Key Key;
-enum types {
-	KEY_RSA1,
-	KEY_RSA,
-	KEY_DSA,
-	KEY_UNSPEC
-};
-enum fp_type {
-	SSH_FP_SHA1,
-	SSH_FP_MD5
-};
-enum fp_rep {
-	SSH_FP_HEX,
-	SSH_FP_BUBBLEBABBLE
-};
+#include "log.h"
 
-/* key is stored in external hardware */
-#define KEY_FLAG_EXT		0x0001
+/* Fatal messages.  This function never returns. */
 
-struct Key {
-	int	 type;
-	int	 flags;
-	RSA	*rsa;
-	DSA	*dsa;
-};
-
-Key	*key_new(int);
-Key	*key_new_private(int);
-void	 key_free(Key *);
-int	 key_equal(Key *, Key *);
-char	*key_fingerprint(Key *, enum fp_type, enum fp_rep);
-char	*key_type(Key *);
-int	 key_write(Key *, FILE *);
-int	 key_read(Key *, char **);
-u_int	 key_size(Key *);
-
-Key	*key_generate(int, u_int);
-Key	*key_from_private(Key *);
-int	 key_type_from_name(char *);
-
-Key	*key_from_blob(u_char *, int);
-int	 key_to_blob(Key *, u_char **, u_int *);
-char	*key_ssh_name(Key *);
-int	 key_names_valid2(const char *);
-
-int	 key_sign(Key *, u_char **, u_int *, u_char *, u_int);
-int	 key_verify(Key *, u_char *, u_int, u_char *, u_int);
-
-#endif
+void
+fatal(const char *fmt,...)
+{
+	va_list args;
+	va_start(args, fmt);
+	do_log(SYSLOG_LEVEL_FATAL, fmt, args);
+	va_end(args);
+	fatal_cleanup();
+}
